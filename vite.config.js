7f927cc732ead377b6cd7230d2c0109f98270b8b
Brandon Yahir Gonzalez <branyahir03@gmail.com>
2026-02-05 23:04:58 -0600

    Configuración final para Netlify

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-
 export default defineConfig({
-  // Borra o comenta la línea de 'base' para Netlify
   plugins: [
     react(),
     tailwindcss(), 
   ],
+  // base: '/BOT-INTELIGENTE/', <-- QUITA ESTO PARA NETLIFY
 })
